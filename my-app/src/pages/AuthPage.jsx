3beae4300ca2b6686bf94e1f5cea1d1d20c6f349
yaun7879-source <yaun7879@gmail.com>
2026-06-11 17:50:38 +0530

    Avoid guest profile 401s

--- my-app/src/pages/AuthPage.jsx
+++ my-app/src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { apiFetch } from '../utils/api';
+import { apiFetch, API_BASE_URL } from '../utils/api';
 
 const EnhancedAuthPage = ({ onLogin }) => {
     const [tab, setTab] = useState('login');
